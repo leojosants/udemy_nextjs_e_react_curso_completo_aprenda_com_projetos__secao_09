ab56519d0e2ee5be304dc091a02e33ac5f2f4be0
Leonardo Santos <leonardojosantos@gmail.com>
2023-08-22 21:48:36 -0300

    layout and title components

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,9 +1,13 @@
+import Layout from "@/components/LayoutComponent";
+
 export default function Home() {
   return (
-    <div className={`
-      flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-600
-    `}>
-      <span className={`text-4xl`}>Texto</span>
-    </div> 
-    );
+    <div className={`flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white`}>
+
+      <Layout title="Cadastro Simples">
+        <span>Conteúdo</span>
+      </Layout>
+
+    </div>
+  );
 };
